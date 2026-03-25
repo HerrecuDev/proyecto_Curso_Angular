110c0a1b1102c4147745e24426714a67a73b46ce
Pablo Herrera <pablojhc15@gmail.com>
2026-03-25 12:09:28 +0100

    buscador categorias listo

--- 10_formularios/src/app/category-list/category-list.component.ts
+++ 10_formularios/src/app/category-list/category-list.component.ts
@@ -1,33 +1,68 @@
 import { Component } from '@angular/core';
 import { Category } from '../category';
 import { CategoryCreateComponent } from '../category-create/category-create.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-category-list',
-  imports: [CategoryCreateComponent],
+  imports: [
+    CategoryCreateComponent,
+    SearchBarComponent
+
+  ],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
 export class CategoryListComponent {
 
-  categories: Category[] = [
+
+  //Primer Arrray de categorias "original"
+  categoriesOriginal: Category[] = [
 
     { id:1 , name: 'Electronica'},
     { id: 2, name: 'Libros'},
     { id: 3, name: 'Bazar'}
 
-  ]
+  ];
+
+  //Creo otro Array para ir mostrando el buscador modificado :
+
+  categories: Category[] = [...this.categoriesOriginal];
+
+  //Recibimos el texto del hijo y filtramos :
+  
+  onSearchChange(searchText: string): void {
+
+    if(!searchText) {
+      //Si esta vacio mostramos las categorias que hay:
+
+      this.categories = [...this.categoriesOriginal];
+    }else{
+      //Filtro por nombre ignorando mayusculas del texto incorporado por el usuario:
+
+      this.categories = this.categoriesOriginal.filter( category =>
+        category.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+      );
+    }
+
+
+
+  }
+ 
 
 
   //Agregamos el componente hijo:
   addCategory(category : { name:string}){
 
     const newCategory = {
-      id : this.categories.length +1,
+      id : this.categoriesOriginal.length +1,
       name : category.name
     };
 
-    this.categories = [...this.categories, newCategory]
+    //Añado ambos arrays:
+    this.categoriesOriginal = [...this.categoriesOriginal, newCategory]
+
+    this.categories = [...this.categories, newCategory];
   }
 
 }
---
+++ 10_formularios/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, output, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [ReactiveFormsModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+
+  //Output para comunicarnos con el padre : 
+  searchChange = output<string>();
+
+  //FormControl para el input
+
+  searchControl = new FormControl('');
+
+
+  //Observable
+  ngOnInit(): void {
+
+      //Escuha los cambios y los emite al padre :
+      this.searchControl.valueChanges.subscribe(value => {
+      this.searchChange.emit( value || '');
+
+
+      });
+  }
+
+}
